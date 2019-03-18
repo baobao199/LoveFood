92bd66646ea6335d9751abf1d930fa8c91bc68b6
Meonix <37493834+Meonix@users.noreply.github.com>
2019-03-18 21:53:16 +0700

    add signout

--- app/src/main/java/com/example/lovefood/MainActivity.java
+++ app/src/main/java/com/example/lovefood/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -59,5 +62,23 @@ public class MainActivity extends AppCompatActivity {
     private void SendUserToLogin() {
         Intent loginIntent = new Intent(MainActivity.this, LogIn.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId()==R.id.logout)
+        {
+            mAuth.signOut();
+            SendUserToLogin();
+        }
+        return true;
     }
 }
